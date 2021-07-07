4c65069ad33c2bfc03e01ae01ff2fe792df810f8
12KJaya <50715606+12KJaya@users.noreply.github.com>
2021-07-07 23:06:26 +0530

    Add files via upload

---
+++ Operations on Array/Deletion Operation/Deletion_From_The_End_of_The_Array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10],size;
+    printf("Enter the size of the array:");
+    scanf("%d",&size);
+    printf("Enter the elements of array:");
+    for(int i=0;i<size;i++)
+    scanf("%d",&a[i]);
+    size--;
+        for(int i=0;i<size;i++)
+        {
+            printf("%d",a[i]);
+        }
+    return 0;
+}
