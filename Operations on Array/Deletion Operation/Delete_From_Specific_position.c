f48eeaa137e0b34b4e7eea7db30dc4b684a4d16c
12KJaya <50715606+12KJaya@users.noreply.github.com>
2021-07-07 22:59:28 +0530

    Add files via upload

---
+++ Operations on Array/Deletion Operation/Delete_From_Specific_position.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10],size,pos;
+    printf("Enter the size of the array:");
+    scanf("%d",&size);
+    printf("Enter the elements of array:");
+    for(int i=0;i<size;i++)
+    scanf("%d",&a[i]);
+    printf("From which position you want to delete:");
+    scanf("%d",&pos);
+    if(pos<=0||pos>size)
+    printf("Invalid Position");
+    else
+    {
+        for(int i=pos-1;i<size;i++)
+        {
+            a[i]=a[i+1];
+        }
+        size--;
+        for(int i=0;i<size;i++)
+        {
+            printf("%d",a[i]);
+        }
+    }
+    return 0;
+}
